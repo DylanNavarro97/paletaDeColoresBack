de3c28664397cc371a1b2e397ded304a9544af33
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-17 22:56:58 -0300

    configuracion index agregada

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import morgan from 'morgan';
+import {fileURLToPath} from 'url'
 
 const app = express();
 
-app.set('port', )
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'))
+})
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get("/", (req, res) => {
+    console.log("alguien solicito algo");
+    res.send('respuesta desde el backend')
+  });
+
+
